55ca0fdd4076d4b0a5d1cb3a1bde8beaed3e2238
Sajnin14 <sajninsaima@gmail.com>
2025-05-02 03:05:26 +0600

    navbar implementation done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,39 @@
 
 const Navbar = () => {
+    const links = <div className="flex gap-8 font-jakarta text-sm ml-[74px]">
+       <li><a>Home</a></li>
+       <li><a>About</a></li>
+       <li><a>Pricing</a></li>
+       <li><a>Consulting</a></li>
+       <li><a>AI Couch</a></li>
+    </div>
     return (
-        <div>
-            <div className="navbar bg-base-100">
+        <div className="w-[1440px] mx-auto">
+            <div className="navbar py-[30px]">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {
+                                links
+                            }
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
+                    {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
+                    <img src="/src/assets/Frame 427321848.png" alt="" />
+
+                    <div className="hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {
+                           links
+                        }
                     </ul>
                 </div>
+                </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="font-bold px-9 py-3 font-manrope text-[#010205] border border-[#010205] rounded-[50px]">Get Started</a>
                 </div>
             </div>
         </div>
